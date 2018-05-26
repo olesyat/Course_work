7f75e4d0a108204a291399573e8941bdd72e767e
olesyat <34340614+olesyat@users.noreply.github.com>
2018-05-26 15:38:22 +0300

    Update App.js

--- examples_app/App.js
+++ examples_app/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Button, TextInput, Image, TouchableOpacity, AsyncStorage} from 'react-native';
+import {StackNavigator} from 'react-navigation';
+import ImgToBase64 from 'react-native-image-base64';
 
 class MainPage extends Component {
     render() {
         return (
             <View style={{backgroundColor: 'white'}}>
                 <Text style={styles.text}>MyMoneyApp</Text>
-                <View style = {styles.buttonContainer}>
+                <View style={styles.buttonContainer}>
                     <Button
                         onPress={() => this.props.navigation.navigate('Login')}
                         title='Press to log in'
-                        color = 'skyblue'
+                        color='skyblue'
                     />
                 </View>
-                <View style = {styles.buttonContainer}>
+                <View style={styles.buttonContainer}>
                     <Button
-                        onPress = {() => this.props.navigation.navigate('Registration')}
+                        onPress={() => this.props.navigation.navigate('Registration')}
                         title='Press to register'
-                        color = 'skyblue'
+                        color='skyblue'
                     />
                 </View>
             </View>
@@ -27,35 +27,56 @@ class MainPage extends Component {
         );
     }
 }
+
+class FuckImage extends Component {
+    render() {
+        return (
+            <Image
+                style={{
+                    alignSelf: 'center',
+                    height: 150,
+                    width: 150,
+                    borderWidth: 1,
+                    borderRadius: 75
+                }}
+                source={{uri: 'content://media/external/images/media/21'}}
+                resizeMode="stretch"
+            />
+
+        )
+    }
+}
+
 class Loginpage extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             username: '',
             password: ''
         }
     }
-    render(){
+
+    render() {
         return (
             <View>
-                <Text style = {styles.text0}>MyMoneyApp</Text>
-                <Text style = {styles.text0}>-LOGIN-</Text>
-                <View style ={styles.container}>
+                <Text style={styles.text0}>MyMoneyApp</Text>
+                <Text style={styles.text0}>-LOGIN-</Text>
+                <View style={styles.container}>
                     <TextInput
-                        style = {styles.textInput} placeholder = 'Username'
-                        onChangeText ={(username)=>this.setState({username})}
+                        style={styles.textInput} placeholder='Username'
+                        onChangeText={(username) => this.setState({username})}
                     />
                 </View>
-                <View style ={styles.container}>
+                <View style={styles.container}>
                     <TextInput
-                        style = {styles.textInput} placeholder = 'Password'
-                        onChangeText ={(password)=>this.setState({password})}
+                        style={styles.textInput} placeholder='Password'
+                        onChangeText={(password) => this.setState({password})}
                     />
-                    <View style = {styles.buttonContainer}>
+                    <View style={styles.buttonContainer}>
                         <Button
-                            onPress = {() => this.props.navigation.navigate('Getinfopage')}
+                            onPress={() => this.props.navigation.navigate('Getinfopage')}
                             title='Log in'
-                            color = 'skyblue'
+                            color='skyblue'
                         />
                     </View>
                 </View>
@@ -65,54 +86,119 @@ class Loginpage extends Component {
     }
 }
 
-class Registerpage extends Component{
-    render(){
-        return(
+class Registerpage extends Component {
+    render() {
+        return (
             <Text>Here will be a registrationpage</Text>
         )
     }
 }
-class NotLoggedin extends Component{
-    render(){
-        return(
+
+class NotLoggedin extends Component {
+    render() {
+        return (
             <Text>You cant be logged in yet</Text>
         )
     }
 }
-class GetInfo extends Component{
-    render(){
-        return(
+
+class GetInfo extends Component {
+    render() {
+        return (
             <View>
-                <Text style = {styles.text0}>MyMoneyApp</Text>
-                <View style = {styles.buttonContainer}>
+                <Text style={styles.text0}>MyMoneyApp</Text>
+                <View style={styles.buttonContainer}>
                     <Button
-                        onPress = {() => this.props.navigation.navigate('NotLogged')}
+                        onPress={() => this.props.navigation.navigate('Enterdata')}
                         title='Ввести данні'
-                        color = 'skyblue'
+                        color='skyblue'
                     />
                 </View>
-                <View style = {styles.buttonContainer}>
+                <View style={styles.buttonContainer}>
                     <Button
-                        onPress = {() => this.props.navigation.navigate('NotLogged')}
+                        onPress={() => this.props.navigation.navigate('NotLogged')}
                         title='Переглянути статистику'
-                        color = 'skyblue'
+                        color='skyblue'
                     />
                 </View>
             </View>
         )
     }
 }
+
+import CameraRollPicker from 'react-native-camera-roll-picker';
+
+class Enterdata extends Component {
+
+
+
+    getSelectedImages(image) {
+        if (image[0])
+            alert(image[0].uri);
+        try {
+            let user = image[0].uri;
+            ImgToBase64.getBase64String(user)
+                .then(base64String => AsyncStorage.setItem('user', base64String))
+                .catch(err => alert(err));
+
+
+        }
+        catch (error) {
+            alert('Unselected')
+        }
+    }
+
+    displayData = async () => {
+        try {
+            let user = await AsyncStorage.getItem('user');
+            alert(user);
+        }
+
+        catch (error) {
+            alert(error);
+        }
+    }
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <Text style={{marginTop: 20}}>
+                    Images selected
+
+                </Text>
+                <TouchableOpacity onPress={this.displayData}>
+                    <Text>CLICK HERE</Text>
+                </TouchableOpacity>
+                <Button
+                    onPress={() => this.props.navigation.navigate('FuckImage')}
+                    title='Сабміт'
+                    color='skyblue'
+                />
+                <CameraRollPicker callback={this.getSelectedImages}
+                                  maximum={1}
+                />
+
+
+            </View>
+        )
+    }
+}
+
 const AppNav = StackNavigator({
     Home: {screen: MainPage},
     Login: {screen: Loginpage},
-    Registration: { screen: Registerpage},
+    Registration: {screen: Registerpage},
     NotLogged: {screen: NotLoggedin},
-    Getinfopage: {screen: GetInfo}
+    Getinfopage: {screen: GetInfo},
+    Enterdata: {screen: Enterdata},
+    FuckImage: {screen: FuckImage}
 });
 const styles = StyleSheet.create({
     text0: {
-      alignSelf: 'center',
-      color: 'blue'
+        alignSelf: 'center',
+        color: 'blue'
     },
     text: {
         color: 'blue',
@@ -137,8 +223,8 @@ const styles = StyleSheet.create({
     }
 });
 
-export default class App extends Component{
-    render(){
-        return <AppNav />;
+export default class App extends Component {
+    render() {
+        return <AppNav/>;
     }
 }
